eee9362fc1a07d1f5b1f56c9fb6cd87be988a8b1
Daniel Arul <danielarul07@gmail.com>
2019-09-01 19:16:24 +0530

    Updated student management project

--- Student Management System/src/Student.java
+++ Student Management System/src/Student.java
@@ -1,48 +1,63 @@
 import java.util.Scanner;
 
 public class Student {
-	private static Scanner scan=new Scanner(System.in);
-	
+	private static Scanner scan = new Scanner(System.in);
+
 	private String firstName;
 	private String lastName;
-	private String coursesEnrolled="";
-   	private int grade;
-//	private String firstName;
-//	
-	
-	
+	private String coursesEnrolled = "";
+	private int studentID = 0;
+	private int grade;
+	private int BalancePayment;
+
 	public Student() {
+		
+		studentID++;
 		System.out.println("Enter the first name of student:");
-		this.firstName=scan.nextLine();
-		
+		this.firstName = scan.nextLine();
+
 		System.out.println("Enter the last name of student:");
-		this.lastName=scan.nextLine();
+		this.lastName = scan.nextLine();
 		
-		System.out.println("Enter the courses the student enrolled: ");
-		
+		int courseNumber=0;
+		System.out.println("Enter the courses the student wishes to enroll.");
+
 		do {
-			System.out.println("Enter Course name (ex to stop entering)");
-			String course=scan.nextLine();
-			
-			if(course.equals("ex")) {
+			courseNumber++;
+			System.out.println("Enter Course"+ courseNumber  +"(ex to stop entering)");
+			String course = scan.nextLine();
+
+			if (course.equals("ex")) {
+				courseNumber--;
 				break;
 			}
 			
-			
-			coursesEnrolled+="\n"+course;
-			
-		}
-			
-		while(1!=0);
-					
-			
-		}
+			coursesEnrolled += "\n" + course;
+		}while (1 != 0);
 		
+		this.BalancePayment=500*courseNumber;
+
+	}
 	
+	public void payTutition() {
+		
+		System.out.println("Enter the amount paid:");
+		int Amount=scan.nextInt();
+		if(Amount>this.BalancePayment) {
+			System.out.println("The Amount paid should be less than or equal to the Balance.");
+		}
+		else {
+				
+		this.BalancePayment-=Amount;
+		System.out.println("Amount Paid:"+Amount+"\nBalancePayment:"+this.BalancePayment);
+	}
+	}
+
+
 	public String toString() {
-		
-		return "Student name:"+firstName+" "+lastName+"\n"+"Courses Enrolled:" + coursesEnrolled ;
-		
-		
+
+		return "Student name:" + firstName + " " + lastName + "\n" + "Courses Enrolled:" + coursesEnrolled+""
+				+ "\nStudent ID:"+studentID+"\nBalance :$"+BalancePayment+"\n";
+
 	}
 }
--- Student Management System/src/main.java
+++ Student Management System/src/main.java
@@ -4,7 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Student std1=new Student();
-		System.out.println(std1);;
+		System.out.println(std1);
+		std1.payTutition();
+		
 		
 	}
 
